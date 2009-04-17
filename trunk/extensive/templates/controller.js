211496ddea3bc98086a38dac2cde65ca5a94a973
Marcus.Schiesser <Marcus.Schiesser@0590f03e-299b-11de-83d2-3711199b9f83>
2009-04-17 08:18:31 +0000

    initial templates

--- trunk/extensive/templates/controller.js
+++ trunk/extensive/templates/controller.js
@@ -1,80 +1,9 @@
-/**
- * @author schiesser
- */
-Ext.ns('efiport.login.controller');
+Ext.ns('{widgetNameSpace}.controller');
 
-efiport.login.controller.login = function(widget){
+{widgetNameSpace}.controller.{controllerName} = function(widget){
     var that = {};
-    
-    that.login = function(email, password){
-		Ext.MessageBox.wait(widget.waitText);
-        var conn = new Ext.data.Connection({
-            url: '/axis2/services/PartnerService/login',
-            exceptionHandler: function(errorText){
-                if (errorText === 'Not unique') {
-					widget.model.email = email;
-					widget.model.password = password;
-			        util.ViewHelper.setView(widget.element, efiport.login.view.firstNameBirthDate);
-                }
-                else {
-					Ext.Msg.alert('Fehler', errorText);
-                }
-            }
-        });
-        widget.model.authPartner.proxy = new Ext.data.HttpProxy(conn);
-        widget.model.authPartner.load({
-            params: {
-                email: email,
-                password: password,
-				domain: widget.domain
-            },
-            callback: that.checkResult('Ihr Passwort ist nicht korrekt oder ihre E-Mail-Adresse ist uns leider nicht bekannt.')
-        });
-    };
-    
-    that.loginUnique = function(firstName, birthDate){
-		Ext.MessageBox.wait(widget.waitText);
-        widget.model.authPartner.proxy = new Ext.data.HttpProxy({
-            url: '/axis2/services/PartnerService/loginUnique'
-        });
-        widget.model.authPartner.load({
-            params: {
-                email: widget.model.email,
-				domain: widget.domain,
-                password: widget.model.password,
-				firstName: firstName,
-				birthDate: birthDate.getElapsed() 
-            },
-            callback: that.checkResult('Sie sind uns leider nicht bekannt. Bitte überprüfen Sie Ihre Angabe oder melden Sie sich neu mit einer anderen E-Mail Adresse an.')
-        });
-    };
-    
-    that.checkResult = function(message){
-        return function(records,options,success){
-			if (success) {
-				Ext.MessageBox.hide();
-				var partner = records[0];
-				if (partner !== undefined) {
-					var id = partner.get('goid');
-					if (id !== undefined && id !== '') {
-						// call defined callback
-						if (widget.callback !== undefined) {
-							widget.callback();
-						}
-						widget.fireEvent('authenticated');
-					}
-					else {
-						Ext.Msg.alert('Fehler', message);
-					}
-				}
-			}
-        };
-    };
-    
-    that.register = function(email){
-        var view = util.ViewHelper.setView(widget.element, efiport.login.view.register);
-        view.emailField.setValue(email);
-    };
-    
+     
+	// TODO add your action functions here
+	
     return that;
 };
--- trunk/extensive/templates/widget/widget.js
+++ trunk/extensive/templates/widget/widget.js
@@ -1,77 +1,24 @@
-/**
- * @author schiesser
- */
-Ext.onReady(function(){
-    Ext.state.Manager.setProvider(new Ext.state.CookieProvider());
-    Ext.QuickTips.init();
-    // turn on validation errors beside the field globally
-    Ext.form.Field.prototype.msgTarget = 'side';
-    Ext.form.VTypes.emailText = "Bei Ihre Eingabe handelt es sich keine E-Mail-Adresse im Format 'benutzer@domain.de'";
-});
+Ext.ns('{widgetNameSpace}');
 
-// register default view of widget
-Ext.ns('efiport');
+{widgetNameSpace}.{widgetClass} = Ext.extend({parentClass}, {
 
-efiport.Login = function(config){
-    Ext.apply(this, config);
-    // create instances
-    this.model = efiport.login.model(this);
-    this.clogin = efiport.login.controller.login(this);
-    this.cregister = efiport.login.controller.register(this);
-	
-	var that = this;
-	util.ViewHelper.setViewFactory(this.element, function(viewClass){
-		return viewClass(that);	
-	});
-
-    this.addEvents("authenticated");
-    efiport.Login.superclass.constructor.call(this);
-
-    if (!this.forceLogin) {
-        this.checkSession();
-    }
-    else {
-        if (!this.doNotRender) {
-            this.render();
-        }
-    }
-};
-
-Ext.extend(efiport.Login, Ext.util.Observable, {
-
-	domain: 'campus.efiport.de',
-    loginText: 'Um weiterzufahren, müssen Sie sich hier einloggen:',
-    newUserText: 'Ja, ich möchte mich als neuer Benutzer anmelden.',
-	commitText : 'Bitte überprüfen Sie Ihre Buchung.',
-	firstNameText: 'Leider konnten wir Ihre E-Mail-Adresse nicht eindeutig zuordnen, daher benötigen wir noch ein paar zusätzliche Angaben von Ihnen, um Sie zu authentifizieren.',
-    newCustomerText: 'Ich bin ein neuer Kunde bei efiport',
-	customerText: 'Ich bin bereits Kunde bei efiport',
-	waitText: 'Bitte warten... wir überprüfen Ihre Daten.',
-	newPartnerOrganisation: '00000039155491~',
-	
-    render: function(){
-		util.ViewHelper.setView(this.element, efiport.login.view.login);
-    },
-    
-    checkSession: function(){
+    constructor: function(config){
+        {widgetNameSpace}.{widgetClass}.superclass.constructor.call(this, config);
         var that = this;
-        Ext.Ajax.simpleRequest({
-            url: '/axis2/services/PartnerService/checkSession',
-			returnType: 'boolean',
-            success: function(sessionValid){
-                if (sessionValid) {
-					that.fireEvent('authenticated');
-					if (that.callback !== undefined) {
-						that.callback();
-					}
-                }
-                else {
-                    if (!that.doNotRender) {
-                        that.render();
-                    }
-                }
-            }
+
+	    that.model = {};
+	    that.controller = {};
+        that.model.app = {widgetNameSpace}.model.app(that);
+    	that.controller.app = {widgetNameSpace}.controller.app(that);
+
+        Extreme.util.ViewHelper.setViewFactory(that, function(viewClass){
+            return viewClass(that);
         });
+		// TODO add view classes here
+        Extreme.util.ViewHelper.setViewClasses(that, []);
+        
+		// TODO set initial view here
+        Extreme.util.ViewHelper.setView(that, undefined);
     }
     
 });
